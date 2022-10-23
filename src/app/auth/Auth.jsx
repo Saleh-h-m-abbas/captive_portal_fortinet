8b42086c410c257f427726855f245a6fc4b9be6a
Saleh hashem Abbas <40650470+Saleh-h-m-abbas@users.noreply.github.com>
2022-10-24 00:12:16 +0300

    last update before flip

--- src/app/auth/Auth.jsx
+++ src/app/auth/Auth.jsx
@@ -9,23 +9,33 @@ import UserButton from "../button/UserLogin";
 import { signInWithGoogle, signInWithFacebook } from "../../firebase/firebase";
 import './AuthCss.css'
 import { FaFacebookF, FaGoogle, FaTwitter, FaApple, FaPhoneAlt } from 'react-icons/fa';
+import { useState } from "react";
 
 const Auth = ({ loginStatusSet, userDataSet }) => {
+    const [flip, setFlip] = useState(false)
     return (
-        <div className="card">
-            <div className="header">To access the Internet<br /> Sign In with</div>
-            <div className="social-group">
-                <div className="social-icons" onClick={signInWithFacebook}>  <FaFacebookF /></div>
-                <div className="social-icons" onClick={() => signInWithGoogle({ loginStatusSet, userDataSet })}>  <FaGoogle /></div>
-                <div className="social-icons" onClick={() => loginStatusSet("1")}>  <FaTwitter /></div>
-                <div className="social-icons">  <FaApple /></div>
-                <div className="social-icons">  <FaPhoneAlt /></div>
+        <>
+            <div className={`card ${flip ? 'flip' : ''}`}>
+                <div className="custom-card front">
+                    <div className="header">To access the Internet<br /> Sign In with</div>
+                    <div className="social-group">
+                        <div className="social-icons" onClick={signInWithFacebook}>  <FaFacebookF /></div>
+                        <div className="social-icons" onClick={() => signInWithGoogle({ loginStatusSet, userDataSet })}>  <FaGoogle /></div>
+                        <div className="social-icons" onClick={() => loginStatusSet("1")}>  <FaTwitter /></div>
+                        <div className="social-icons">  <FaApple /></div>
+                        <div className="social-icons" onClick={() => setFlip(!flip)}>  <FaPhoneAlt /></div>
+                    </div>
+                    <hr />
+                    <div className="header">or<br />Premium Sign In </div>
+                    <UserButton onClick={() => alert("Hello")} />
+
+                </div>
+                {/* <div className="back">
+                    <div className="header">PhoneNumber</div>
+
+                </div> */}
             </div>
-            <hr />
-            <div className="header">or<br />Premium Sign In </div>
-            <UserButton onClick={() => alert("Hello")} />
-            
-        </div>
+        </>
     )
 }
 
