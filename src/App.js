f490621a61777601ea6e4b45163f4e37381ad7db
Saleh hashem Abbas <40650470+Saleh-h-m-abbas@users.noreply.github.com>
2022-11-11 02:50:05 +0200

    add terms page

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import { Routes,Route } from 'react-router-dom';
 import './App.css';
 import Home from './app/home/Home';
 import Privacy from './web_pages/Privacy/Privacy';
+import Terms from './web_pages/Privacy/Terms';
 function App() {
 
   return (
     <Routes>
       <Route path='/' element={<Home />}/>
       <Route path='/privacy' element={<Privacy />} />
-    {/* <div className="App">
-      <Home />
-    </div> */}
+      <Route path='/terms' element={<Terms />} />
     </Routes>
   );
 }
